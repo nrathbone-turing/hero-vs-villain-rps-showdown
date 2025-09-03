3a4c9aaf2ce4d0122baf69f27f50c6b9530361c9
Nick Rathbone <nrathbone@joinhandshake.com>
2025-09-02 22:23:58 -0600

    refactor(server): move static heroes into api/popularHeroes.js

---
+++ server/api/popularHeroes.js
@@ -0,0 +1,9 @@
+// server/api/popularHeroes.js
+// Static list of popular heroes (can later be swapped for DB/API calls)
+
+export const popularHeroes = [
+  { id: 70, name: "Batman", powerstats: { strength: "50" } },
+  { id: 644, name: "Superman", powerstats: { strength: "100" } },
+  { id: 720, name: "Wonder Woman", powerstats: { strength: "85" } },
+  { id: 620, name: "Spider-Man", powerstats: { strength: "55" } },
+]
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { popularHeroes } from './api/popularHeroes.js'
 
 dotenv.config()
 
@@ -35,17 +36,9 @@ app.get('/api/hero/:id', async (req, res) => {
   }
 })
 
-// Temporary static heroes for list endpoint
-const staticHeroes = [
-  { id: 70, name: "Batman", powerstats: { strength: "50" } },
-  { id: 644, name: "Superman", powerstats: { strength: "100" } },
-  { id: 720, name: "Wonder Woman", powerstats: { strength: "85" } },
-  { id: 620, name: "Spider-Man", powerstats: { strength: "55" } },
-]
-
-// Popular heroes endpoint (stub)
+// Popular heroes endpoint
 app.get('/api/popular-heroes', (req, res) => {
-  res.json(staticHeroes)
+  res.json(popularHeroes)
 })
 
 if (process.env.NODE_ENV !== 'test') {
