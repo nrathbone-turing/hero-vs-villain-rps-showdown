8328ce5325c059ad41471e15247f3c49691839c9
Nick Rathbone <nrathbone@joinhandshake.com>
2025-09-02 23:01:41 -0600

    chore(api): update popularHeroes stub to include full powerstats

--- server/api/popularHeroes.js
+++ server/api/popularHeroes.js
@@ -2,8 +2,52 @@
 // Static list of popular heroes (can later be swapped for DB/API calls)
 
 export const popularHeroes = [
-  { id: 70, name: "Batman", powerstats: { strength: "50" } },
-  { id: 644, name: "Superman", powerstats: { strength: "100" } },
-  { id: 720, name: "Wonder Woman", powerstats: { strength: "85" } },
-  { id: 620, name: "Spider-Man", powerstats: { strength: "55" } },
+  {
+    id: 70,
+    name: "Batman",
+    powerstats: {
+      intelligence: "100",
+      strength: "26",
+      speed: "27",
+      durability: "50",
+      power: "47",
+      combat: "100"
+    }
+  },
+  {
+    id: 644,
+    name: "Superman",
+    powerstats: {
+      intelligence: "94",
+      strength: "100",
+      speed: "100",
+      durability: "100",
+      power: "100",
+      combat: "85"
+    }
+  },
+  {
+    id: 720,
+    name: "Wonder Woman",
+    powerstats: {
+      intelligence: "88",
+      strength: "100",
+      speed: "79",
+      durability: "100",
+      power: "100",
+      combat: "100"
+    }
+  },
+  {
+    id: 620,
+    name: "Spider-Man",
+    powerstats: {
+      intelligence: "90",
+      strength: "55",
+      speed: "67",
+      durability: "75",
+      power: "74",
+      combat: "85"
+    }
+  }
 ]
